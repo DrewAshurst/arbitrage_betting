77a4d1e8acaf9c85ea0d4d30b19f24f69b5dfe98
Drew <94008915+Dayrue@users.noreply.github.com>
2023-02-06 16:26:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     data = dataLocaters.findGames()
     
     with open('collegeBasketball.csv', 'w', newline='') as f:  # You will need 'wb' mode in Python 2.x
-        w = csv.DictWriter(f, data[i].keys())
+        w = csv.DictWriter(f, data[0].keys())
         w.writeheader()
         for i in data:
             w.writerow(data[i])
